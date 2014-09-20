1695b0f6e0f68761c46aa0692decda6ee58dbda9
Ashley <ashleykwtam@gmail.com>
2014-09-20 12:52:05 -0400

    added select feature to quality and awkwardness for events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to :back
+      redirect_to events_path
     else
       render :new
     end
