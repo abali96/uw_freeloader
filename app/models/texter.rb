4fb61164403757857977dd77d509cd00c7b97539
Arjun <arjun.bali96@gmail.com>
2014-09-20 21:43:13 -0400

    text message sends 15 minutes before event starts

--- app/models/texter.rb
+++ app/models/texter.rb
@@ -2,7 +2,7 @@ class Texter
   include TwilioClient
 
   def send_event_notification(latitude, longitude, name, location, start_time, food_type)
-    body = "In 15 minutes: FREE #{food_type} at #{location} starting in 15 minutes. (Event name: #{name}) Get directions: https://www.google.com/maps?saddr=My+Location&daddr=#{latitude},#{longitude}"
+    body = "In 15 minutes: FREE #{food_type} at #{location} starting in at #{start_time.strftime('%I:%M %p')}. (Event name: #{name}) Get directions: https://www.google.com/maps?saddr=My+Location&daddr=#{latitude},#{longitude}"
     twilio_client.messages.create(
     :from => '+12898073438',
     :to => '+16478904632',
