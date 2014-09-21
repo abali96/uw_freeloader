29c589d55722825e3651939ecdf31363882d9a8f
Arjun <arjun.bali96@gmail.com>
2014-09-21 09:49:46 -0400

    fixed food type event form

--- app/models/texter.rb
+++ app/models/texter.rb
@@ -3,7 +3,7 @@ class Texter
 
   def send_event_notification(latitude, longitude, name, location, start_time, food_type)
     User.where.not(phone_number: nil).each do |user|
-      body = "In 15 minutes: FREE #{food_type} at #{location} starting at #{start_time.strftime('%I:%M %p')}. (Event name: #{name}) Get directions: http://www.google.com/maps?saddr=My+Location&daddr=#{latitude},#{longitude}"
+      body = "Free #{food_type} at #{location} starting at #{start_time.strftime('%I:%M %p')} (15 minutes from now) | Event: #{name} | Get directions: http://www.google.com/maps?saddr=My+Location&daddr=#{latitude},#{longitude}"
       twilio_client.messages.create(
       :from => '+12898073438',
       :to => user.phone_number,
